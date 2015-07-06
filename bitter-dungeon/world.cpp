514564d65a01f0dee87c0ef2f936c20b0b367186
shine-spike <jack_mckinney98@hotmail.com>
2015-07-06 19:42:28 +0930

    Added some resources and began to code the tile map

---
+++ bitter-dungeon/world.cpp
@@ -0,0 +1,16 @@
+//
+//  world.cpp
+//  bitter-dungeon
+//
+//  Created by Jack McKinney on 6/07/2015.
+//  Copyright (c) 2015 Jack McKinney. All rights reserved.
+//
+
+#include "world.h"
+
+class World {
+    char [<#instance variables#>
+    
+public:
+    <#member functions#>
+};
---
+++ bitter-dungeon/world.h
@@ -0,0 +1,14 @@
+//
+//  world.h
+//  bitter-dungeon
+//
+//  Created by Jack McKinney on 6/07/2015.
+//  Copyright (c) 2015 Jack McKinney. All rights reserved.
+//
+
+#ifndef __bitter_dungeon__world__
+#define __bitter_dungeon__world__
+
+#include <stdio.h>
+
+#endif /* defined(__bitter_dungeon__world__) */
